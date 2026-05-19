b4292d4e4bec1b2c49f676ef178f7a838a343650
QFINHUB Dev <dev@qfinhub.com>
2026-05-19 21:29:52 +0300

    fix: robots.txt sitemap URL use www.qfinhub.com

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -6,6 +6,6 @@ export default function robots(): MetadataRoute.Robots {
       userAgent: "*",
       allow: "/",
     },
-    sitemap: "https://qfinhub.com/sitemap.xml",
+    sitemap: "https://www.qfinhub.com/sitemap.xml",
   };
 }
