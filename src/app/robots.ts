b74a08f1dd951fb72b44b2fe3c597c56d8243928
QFINHUB Dev <dev@qfinhub.com>
2026-05-21 21:38:41 +0300

    seo: disallow trailing-slash URLs in robots.txt to save crawl budget

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -2,10 +2,13 @@ import type { MetadataRoute } from "next";
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: {
-      userAgent: "*",
-      allow: "/",
-    },
+    rules: [
+      {
+        userAgent: "*",
+        allow: "/",
+        disallow: "/*/$",
+      },
+    ],
     sitemap: [
       "https://www.qfinhub.com/sitemap.xml",
       "https://www.qfinhub.com/scenario/sitemap.xml",
